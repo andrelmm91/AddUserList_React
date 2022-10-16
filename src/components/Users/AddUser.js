628516887a18f01969d50c5e149b524c1b3f3e8d
unknown <andre.lmm91@gmail.com>
2022-10-16 18:02:32 +0200

    adding user interface

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,19 @@
-
 import React from "react";
 
-function AddUser(){
-    function addUserHandler(event) {
-        event.preventDefault();
-    };
+function AddUser() {
+  function addUserHandler(event) {
+    event.preventDefault();
+  }
 
+  return (
     <form onSubmit={addUserHandler}>
-        <label htmlFor="username">User Name</label>
-        <input id="username" type="text" />
-        <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
-        <buttton type="submit">Add User</buttton>
+      <label htmlFor="username">User Name</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age</label>
+      <input id="age" type="number" />
+      <buttton type="submit">Add User</buttton>
     </form>
-};
+  );
+}
 
 export default AddUser;
