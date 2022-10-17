3356ddd98d23b0225553a5e430d8da4e14f4d6e3
unknown <andre.lmm91@gmail.com>
2022-10-17 21:22:50 +0200

    adding portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,26 +1,38 @@
 import React from "react";
-import Wrappers from "../helpers/Wrappers";
+import ReactDOM from "react-dom/client";
 import Button from "./Button";
 import Card from "./Card";
 import classes from "./ErrorModal.module.css";
 
+// adding portal here to the HTML root
+function Backdrop(props) {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+}
+// adding portal here to the HTML root
+function ModalOverlay(props) {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+}
 // there are two onClick, one if clicking outside the box and another clicking on the button.
 function ErrorModal(props) {
   return (
-    <Wrappers>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </Wrappers>
+    <React.Fragment>
+      {ReactDOM.createPorta(
+        <Backdrop onConfirm={props.onConfirm} />, 
+        document.getElementById("backdrp-root")
+      )}
+    </React.Fragment>
   );
 }
 
